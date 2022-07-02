e1b375cf7b7aa4110d24c6fd54d5be5154e31b49
L0voj <wonjjw98@gmail.com>
2022-07-02 21:56:20 +0900

    pathfinder

--- maze_main.cpp
+++ maze_main.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 #include <cstdlib>
 #include <string>
-#include "mazeframe.h"	//mazeframe here
-#include "mazeframe.cpp"
-
+#include "maze_frame/main_frame.h"	//mazeframe here
+#include "maze_frame/frame_init.cpp"
+#include "maze_frame/maze_check.cpp"
+#include "maze_frame/pathfinder.cpp"
 using namespace std;
 
 int main(int argc, char **argv){
-	//cout<<argc;
-	if(argc>1){
-		mazeframe hello(argv);
-		hello.ckeckfield();
-	}
-	else{
-		mazeframe hello;
-		hello.ckeckfield();
-	}
+	system("clear");
+	mazeframe hello(argc,argv);
+	hello.ckeckfield();
+	pathfinding path(argc,argv);
+	path.solution_check();
 }
