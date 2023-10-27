44cecceae1fca0b67853be59af1650f81bf59807
l0voj <wonjjw98@gmail.com>
2023-10-27 14:06:14 +0900

    230127_still_alive

---
+++ test/0111.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double a,b,c;
+	a=0.1;b=1.1;c=1.2;
+	cout<<a+b-c<<endl;
+	if(a+b-c==0){
+		cout<<"true"<<endl;
+	}
+	else{
+		cout<<"false"<<endl;
+	}
+}
